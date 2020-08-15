5dfd7ad4e865c7f0909137a2190ac26fdf47fadd
Spencer Ong <recneps@fb.com>
2020-08-16 00:44:29 +0100

    Rename classes + remove tutorial comments

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:sqflite/sqflite.dart';
 import 'package:intl/intl.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Plantz());
 }
 
 void createTables(Database db) {
@@ -20,11 +20,7 @@ void createTables(Database db) {
 
 Future<Database> getDatabase() async {
   return openDatabase(
-    // Set the path to the database. Note: Using the `join` function from the
-    // `path` package is best practice to ensure the path is correctly
-    // constructed for each platform.
     join(await getDatabasesPath(), 'plantz.db'),
-    // When the database is first created, create a table to store dogs.
     onUpgrade: (db, oldversion, newVersion) {
       createTables(db);
     },
@@ -35,40 +31,22 @@ Future<Database> getDatabase() async {
   );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Plantz extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Plantz',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => MyHomePage(title: 'Plantz'),
+        '/': (context) => MainPage(title: 'Plantz'),
       },
       onGenerateRoute: (settings) {
-        // If you push the PassArguments route
         if (settings.name == '/detail') {
-          // Cast the arguments to the correct type: ScreenArguments.
           final plant = settings.arguments;
-
-          // Then, extract the required data from the arguments and
-          // pass the data to the correct screen.
           return MaterialPageRoute(
             builder: (context) {
               return PlantDetailPage(
@@ -189,22 +167,13 @@ class _PlantDetailPageState extends State<PlantDetailPage> {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MainPage extends StatefulWidget {
+  MainPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainPageState createState() => _MainPageState();
 }
 
 class Plant {
@@ -216,7 +185,7 @@ class Plant {
   Plant(this.name, this.id, this.frequency, this.ts);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MainPageState extends State<MainPage> {
   TextEditingController frequencyController;
   TextEditingController addController;
   List<Plant> plants = [];
@@ -289,13 +258,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: RefreshIndicator(
           child: ListView(
               children: plants
@@ -384,7 +349,7 @@ class _MyHomePageState extends State<MyHomePage> {
         },
         tooltip: 'Add plant',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
